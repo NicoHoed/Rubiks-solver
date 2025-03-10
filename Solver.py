b546fa33b1de44404a84e309b331433e27c3ee36
NicoHoed <nico.hoed@gmail.com>
2025-03-10 07:09:27 +0100

    First commit (Serial comm. + solver python file)

---
+++ Solver.py
@@ -0,0 +1,128 @@
+##################################################################
+# Simple code: Rubik capture
+##################################################################
+
+import cv2
+import kociemba
+
+
+# Start camera
+vid = cv2.VideoCapture(0)
+
+# Create empty list1
+rubik = []
+core = []
+
+for i in range(1):
+    while 1:
+        # Read camera
+        ret, frame = vid.read()
+        # Draw rectangle on captured image
+        cv2.rectangle(frame, (100, 100), (400, 400), (0, 255, 0), 2)
+
+        # Show images
+        cv2.imshow("camera", frame)
+
+        # Break by pressing "z" (not "Z")
+        if cv2.waitKey(10) == ord('z'):
+            # Get color from 9 positions
+            color1 = frame[150, 150]
+            color2 = frame[150, 250]
+            color3 = frame[150, 350]
+            color4 = frame[250, 150]
+            color5 = frame[250, 250]
+            color6 = frame[250, 350]
+            color7 = frame[350, 150]
+            color8 = frame[350, 250]
+            color9 = frame[350, 350]
+
+            # Add color to Rubik list
+            rubik.append(color1)
+            rubik.append(color2)
+            rubik.append(color3)
+            rubik.append(color4)
+            rubik.append(color5)
+            rubik.append(color6)
+            rubik.append(color7)
+            rubik.append(color8)
+            rubik.append(color9)
+
+            # Add color to Core list
+            core.append(color5)
+
+            # Close window
+            cv2.destroyAllWindows()
+            break
+
+# Close window
+cv2.destroyAllWindows()
+# Release camera
+vid.release()
+
+# Color list of 54 squares
+print("Color list of 54 squares:")
+print(rubik)
+# Color list of 6 faces
+print("Color list of 6 faces:")
+print(core)
+
+# ------Compare color of each square to decide that is the color of which face-------------
+
+colors = {
+    "Y": (0, 255, 255),
+    "R": (0, 0, 255),
+    "W": (255, 255, 255),
+    "B": (255, 0, 0),
+    "O": (0, 150, 255),
+    "G": (0, 255, 0)
+}
+
+
+def compare(bgr):
+    best = ("", 999)
+
+    for color in colors:
+        col = colors[color]
+
+        temp = abs(bgr[0] - int(col[0])) + abs(bgr[1] - int(col[1])) + abs(bgr[2] - int(col[2]))
+        if temp < best[1]:
+            best = (color, temp)
+    return best[0]
+
+
+cube = ""
+
+for squares in rubik:
+
+    cube = cube + (compare(squares.tolist()))
+
+print('****************************')
+print(cube)
+
+cubeConverted = ''
+
+for char in cube:
+    if char == 'G':
+        cubeConverted += 'L'
+    elif char == 'W':
+        cubeConverted += 'F'
+    elif char == 'Y':
+        cubeConverted += 'B'
+    elif char == 'R':
+        cubeConverted += 'D'
+    elif char == 'B':
+        cubeConverted += 'R'
+    elif char == 'O':
+        cubeConverted += 'U'
+
+print('****************************')
+print(cubeConverted)
+print('****************************')
+
+# ------Apply algorithm using data of the Rubik cube above to get the solving moves--------
+
+t = kociemba.solve(cubeConverted)
+
+# ------Apply algorithm using data of the Rubik cube above to get the solving moves--------
+
+print(t)
---
+++ serialComm.py
@@ -0,0 +1,33 @@
+##################################################################
+# Simple code: Serial communication
+##################################################################
+
+import serial
+import time
+
+
+# Example Moves:
+t = "R L U D"
+
+# Open Serial port
+arduino = serial.Serial(port='COM7', baudrate=115200)
+
+# Wait 10 seconds
+time.sleep(10)
+
+# Send word "MOVE" to Arduino
+arduino.write(bytes("MOVE",   'utf-8'))
+
+# Wait 10 seconds
+time.sleep(10)
+
+# Send solving Moves
+arduino.write(bytes(t,   'utf-8'))
+
+# Wait 60 seconds
+time.sleep(60)
+
+# Close Serial port
+arduino.close()
+
+
